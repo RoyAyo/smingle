332db186858dd18f7521b0040a311e09e87b3aeb
RoyAyo <roylayindeay0@gmail.com>
2020-01-19 22:24:34 +0100

    added more questionnaires on about you

--- resources/views/pages/pages/general2.blade.php
+++ resources/views/pages/pages/general2.blade.php
@@ -99,127 +99,127 @@
 			<div class="q">
 				<p>Men should be superiors in relationships</p>
 				<span>
-					<input type="radio" name="gen1" id="opt1" class="btnQ">
-					<label for="opt1"><span>Hello World</span></label>
+					<input type="radio" name="gen6" id="opt1" class="btnQ">
+					<label for="opt1"><span>This question is stupid</span></label>
 				<span>
-					<input type="radio" name="gen1" id="opt2" class="btnQ">
-					<label for="opt2"><span>Hello World</span></label>
+					<input type="radio" name="gen6" id="opt2" class="btnQ">
+					<label for="opt2"><span>Definitely Not(shared responsibilities)</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt3" class="btnQ">
-					<label for="opt3"><span>Hello World</span></label>
+					<input type="radio" name="gen6" id="opt3" class="btnQ">
+					<label for="opt3"><span>Yes, if it comes down to it</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt4" class="btnQ">
-					<label for="opt4"><span>Hello World</span></label>
-				</span>
-			</div>
-			<div class="q">
-				<p></p>
-				<span>
-					<input type="radio" name="gen1" id="opt1" class="btnQ">
-					<label for="opt1"><span>Hello World</span></label>
-				<span>
-					<input type="radio" name="gen1" id="opt2" class="btnQ">
-					<label for="opt2"><span>Hello World</span></label>
-				</span>
-				<span>
-					<input type="radio" name="gen1" id="opt3" class="btnQ">
-					<label for="opt3"><span>Hello World</span></label>
-				</span>
-				<span>
-					<input type="radio" name="gen1" id="opt4" class="btnQ">
-					<label for="opt4"><span>Hello World</span></label>
+					<input type="radio" name="gen6" id="opt4" class="btnQ">
+					<label for="opt4"><span>Men should be superiors as more responsibilities fall on them</span></label>
 				</span>
 			</div>
 			<div class="q">
-				<p></p>
+				<p>Music?</p>
 				<span>
-					<input type="radio" name="gen1" id="opt1" class="btnQ">
-					<label for="opt1"><span>Hello World</span></label>
+					<input type="radio" name="gen7" id="opt1" class="btnQ">
+					<label for="opt1"><span>Old School</span></label>
 				<span>
-					<input type="radio" name="gen1" id="opt2" class="btnQ">
-					<label for="opt2"><span>Hello World</span></label>
+					<input type="radio" name="gen7" id="opt2" class="btnQ">
+					<label for="opt2"><span>Love songs</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt3" class="btnQ">
-					<label for="opt3"><span>Hello World</span></label>
+					<input type="radio" name="gen7" id="opt3" class="btnQ">
+					<label for="opt3"><span>Rap/Trap</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt4" class="btnQ">
-					<label for="opt4"><span>Hello World</span></label>
-				</span>
-			</div>
-			<div class="q">
-				<p></p>
-				<span>
-					<input type="radio" name="gen1" id="opt1" class="btnQ">
-					<label for="opt1"><span>Hello World</span></label>
-				<span>
-					<input type="radio" name="gen1" id="opt2" class="btnQ">
-					<label for="opt2"><span>Hello World</span></label>
-				</span>
-				<span>
-					<input type="radio" name="gen1" id="opt3" class="btnQ">
-					<label for="opt3"><span>Hello World</span></label>
-				</span>
-				<span>
-					<input type="radio" name="gen1" id="opt4" class="btnQ">
-					<label for="opt4"><span>Hello World</span></label>
+					<input type="radio" name="gen7" id="opt4" class="btnQ">
+					<label for="opt4"><span>Fast Paced Jams</span></label>
 				</span>
 			</div>
 			<div class="q">
-				<p></p>
+				<p>Yahoo Boys?</p>
 				<span>
-					<input type="radio" name="gen1" id="opt1" class="btnQ">
-					<label for="opt1"><span>Hello World</span></label>
+					<input type="radio" name="gen8" id="opt1" class="btnQ">
+					<label for="opt1"><span>They were pushed to the edge by country</span></label>
 				<span>
-					<input type="radio" name="gen1" id="opt2" class="btnQ">
-					<label for="opt2"><span>Hello World</span></label>
+					<input type="radio" name="gen8" id="opt2" class="btnQ">
+					<label for="opt2"><span>I could care less</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt3" class="btnQ">
-					<label for="opt3"><span>Hello World</span></label>
+					<input type="radio" name="gen8" id="opt3" class="btnQ">
+					<label for="opt3"><span>Stupid people</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt4" class="btnQ">
-					<label for="opt4"><span>Hello World</span></label>
-				</span>
-			</div>
-			<div class="q">
-				<p></p>
-				<span>
-					<input type="radio" name="gen1" id="opt1" class="btnQ">
-					<label for="opt1"><span>Hello World</span></label>
-				<span>
-					<input type="radio" name="gen1" id="opt2" class="btnQ">
-					<label for="opt2"><span>Hello World</span></label>
-				</span>
-				<span>
-					<input type="radio" name="gen1" id="opt3" class="btnQ">
-					<label for="opt3"><span>Hello World</span></label>
-				</span>
-				<span>
-					<input type="radio" name="gen1" id="opt4" class="btnQ">
-					<label for="opt4"><span>Hello World</span></label>
+					<input type="radio" name="gen8" id="opt4" class="btnQ">
+					<label for="opt4"><span>Something serious really needs to be done about them.</span></label>
 				</span>
 			</div>
 			<div class="q">
-				<p></p>
+				<p>Travelling the world?</p>
 				<span>
-					<input type="radio" name="gen1" id="opt1" class="btnQ">
-					<label for="opt1"><span>Hello World</span></label>
+					<input type="radio" name="gen9" id="opt1" class="btnQ">
+					<label for="opt1"><span>I want to see every corner I possibly can</span></label>
 				<span>
-					<input type="radio" name="gen1" id="opt2" class="btnQ">
-					<label for="opt2"><span>Hello World</span></label>
+					<input type="radio" name="gen9" id="opt2" class="btnQ">
+					<label for="opt2"><span>I have a few places in mind</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt3" class="btnQ">
-					<label for="opt3"><span>Hello World</span></label>
+					<input type="radio" name="gen9" id="opt3" class="btnQ">
+					<label for="opt3"><span>Not thought about it</span></label>
 				</span>
 				<span>
-					<input type="radio" name="gen1" id="opt4" class="btnQ">
-					<label for="opt4"><span>Hello World</span></label>
+					<input type="radio" name="gen9" id="opt4" class="btnQ">
+					<label for="opt4"><span>I am comfy in my small corner</span></label>
+				</span>
+			</div>
+			<div class="q">
+				<p>Friends?</p>
+				<span>
+					<input type="radio" name="gen10" id="opt1" class="btnQ">
+					<label for="opt1"><span>Bitter people that want to see you down</span></label>
+				<span>
+					<input type="radio" name="gen10" id="opt2" class="btnQ">
+					<label for="opt2"><span>I don't have them generally</span></label>
+				</span>
+				<span>
+					<input type="radio" name="gen10" id="opt3" class="btnQ">
+					<label for="opt3"><span>Very few people I can call friends</span></label>
+				</span>
+				<span>
+					<input type="radio" name="gen10" id="opt4" class="btnQ">
+					<label for="opt4"><span>Best thing in my life</span></label>
+				</span>
+			</div>
+			<div class="q">
+				<p>Money I want?</p>
+				<span>
+					<input type="radio" name="gen11" id="opt1" class="btnQ">
+					<label for="opt1"><span>The one that fears devil</span></label>
+				<span>
+					<input type="radio" name="gen11" id="opt2" class="btnQ">
+					<label for="opt2"><span>Plenty</span></label>
+				</span>
+				<span>
+					<input type="radio" name="gen11" id="opt3" class="btnQ">
+					<label for="opt3"><span>Adequate</span></label>
+				</span>
+				<span>
+					<input type="radio" name="gen11" id="opt4" class="btnQ">
+					<label for="opt4"><span>Average</span></label>
+				</span>
+			</div>
+			<div class="q">
+				<p>School?</p>
+				<span>
+					<input type="radio" name="gen12" id="opt1" class="btnQ">
+					<label for="opt1"><span>Waste of time and money generally</span></label>
+				<span>
+					<input type="radio" name="gen12" id="opt2" class="btnQ">
+					<label for="opt2"><span>It works/worked for others, just not me</span></label>
+				</span>
+				<span>
+					<input type="radio" name="gen12" id="opt3" class="btnQ">
+					<label for="opt3"><span>It works/worked for me</span></label>
+				</span>
+				<span>
+					<input type="radio" name="gen12" id="opt4" class="btnQ">
+					<label for="opt4"><span>Best thing happening/that happened to me</span></label>
 				</span>
 			</div>
 			<div class="q">
