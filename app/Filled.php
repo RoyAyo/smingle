edce78975997ef070ca638973c007acb37ed8197
RoyAyo <roylayindeay0@gmail.com>
2020-02-05 07:09:53 +0100

    deleted the matches model + -m

--- app/Filled.php
+++ app/Filled.php
@@ -7,9 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Filled extends Model
 {
     protected $fillable = [
-        'user_id', 'general', 'relationship','random','situation','movie','music','career'
+        'user_id', 'general'
     ];
-
     public function user(){
     	return $this->belongsTo('App\User');
     }
